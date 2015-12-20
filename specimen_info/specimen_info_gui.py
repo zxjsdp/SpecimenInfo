5e2b6df8b5e86741e789557b954a71a5c80c8b18
zxjsdp <zxjsdp@gmail.com>
2015-12-20 15:05:19 +0000

    Bump version to 1.1.6

--- specimen_info/specimen_info_gui.py
+++ specimen_info/specimen_info_gui.py
@@ -65,7 +65,7 @@ else:
     raise ImportError('Cannot identify your Python version.')
 
 
-__version__ = "v1.1.5"
+__version__ = "v1.1.6"
 
 __all__ = ['Query', 'write_to_xlsx_file', 'write_to_sqlite3',
            'gui_main']
